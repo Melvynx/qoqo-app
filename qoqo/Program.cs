8a6589c4c6b7e06bf82134dd18d74fdb629fa4af
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2021-12-17 11:09:21 +0100

    deps(api): add newtonsoft and setup it

--- qoqo/Program.cs
+++ qoqo/Program.cs
@@ -1,10 +1,20 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using qoqo.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.ContractResolver = new DefaultContractResolver
+            {NamingStrategy = new CamelCaseNamingStrategy()};
+        options.SerializerSettings.Formatting = Formatting.Indented;
+        options.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Local;
+    }
+);
 
 builder.Services.AddDbContext<QoqoContext>();
 
