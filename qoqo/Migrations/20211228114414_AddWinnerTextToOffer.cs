5a9e8823777fee3de7a8c81ea3eb40d72ea82f32
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2021-12-28 13:12:42 +0100

    db(migration) move column

---
+++ qoqo/Migrations/20211228114414_AddWinnerTextToOffer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace qoqo.Migrations
+{
+    public partial class AddWinnerTextToOffer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WinnerText",
+                table: "Offers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WinnerText",
+                table: "Offers");
+        }
+    }
+}
