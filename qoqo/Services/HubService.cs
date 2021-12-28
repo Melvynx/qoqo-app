1958c4f2e918b86e1c35bd94061297c4fedde142
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2021-12-28 13:36:51 +0100

    add(service): Hub for the websocket

--- qoqo/Services/HubService.cs
+++ qoqo/Services/HubService.cs
@@ -1,6 +1,71 @@
+using System.Linq;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using qoqo.DataTransferObjects;
+using qoqo.Hubs;
+using qoqo.Model;
+
 namespace qoqo.Services;
 
 public class HubService
 {
+    private readonly QoqoContext _context;
+    private readonly IHubContext<OfferHub> _hubContext;
+
+    public HubService(QoqoContext qoqoContext, IHubContext<OfferHub> hubContext)
+    {
+        _context = qoqoContext;
+        _hubContext = hubContext;
+    }
+
+    public async Task<ClickEventResult> Finish(ClickDto clickDto, Click click)
+    {
+        var order = new Order {
+            OfferId = click.OfferId,
+            UserId = click.UserId,
+            Status = OrderStatus.PENDING
+        };
+        await _context.Orders.AddAsync(order);
+
+        var offer = await _context.Offers.FindAsync(click.OfferId);
+        if (offer == null)
+        {
+            return new ClickEventResult(false);
+        }
+        
+        var user = await _context.Users
+            .Select(u => new {u.UserId, u.UserName})
+            .FirstAsync(u => u.UserId == click.UserId);
+
+        offer.IsOver = true;
+        offer.WinnerText = await GetWinnerSentence(user.UserId, offer.Id, offer.ClickObjective, user.UserName);
+
+        await _context.SaveChangesAsync();
+      
+        var result = new ClickEventFinishResult(user.UserId, user.UserName, offer.WinnerText, clickDto.ClickCount);
+        await SendAsync("FINISH", result);
+        return new ClickEventResult(true);
+    }
     
+    public async Task<ClickEventResult> Click(ClickDto clickDto)
+    {
+        await SendAsync("CLICK", clickDto);
+        return new ClickEventResult(false);
+    }
+
+    public async Task<string> GetWinnerSentence(int userId, int offerId, int clickObjective, string userName)
+    {
+        var winnerClickCount = await _context.Clicks
+            .Where(c => c.UserId == userId && c.OfferId == offerId)
+            .CountAsync();
+        
+        var winnerPercentage = winnerClickCount * 100 / clickObjective;
+        
+        return $"<b>@{userName}</b> has wine the challenge! He is the <b>{clickObjective}</b> to click on the button. The click <b>{winnerClickCount} time</b>, so he reach <b>{winnerPercentage}%</b> of the total click.";
+    }
+    
+    private async Task SendAsync(string key, object obj)
+    {
+        await _hubContext.Clients.All.SendAsync(key, JsonService.Serialize(obj));
+    }
 }
