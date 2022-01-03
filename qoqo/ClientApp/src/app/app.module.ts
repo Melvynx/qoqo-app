4c5f26bf5fbccf36eb01d1cf6874b0d7664c5a5f
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-01-03 16:33:59 +0100

    ui(admin): fix ui of admin compoentn like Digitec

--- qoqo/ClientApp/src/app/app.module.ts
+++ qoqo/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -19,13 +19,20 @@ import { AuthComponent } from './auth/auth.component';
 import { ViewComponent } from './auth/view/view.component';
 import { AuthButtonComponent } from './auth/button/auth-button.component';
 import { AuthGuard } from './guard/auth.guard';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AuthClickComponent } from './auth/auth-click/auth-click.component';
 import { AuthOrderComponent } from './auth/auth-order/auth-order.component';
 import { AdminComponent } from './admin/admin.component';
 import { AdminGuard } from './guard/admin.guard';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { OffersComponent } from './admin/offers/offers.component';
+import { OfferCrudComponent } from './admin/offers/crud/offer-crud.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { OrdersComponent } from './admin/orders/orders.component';
 
 const ROUTES: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -61,7 +68,22 @@ const ROUTES: Routes = [
     path: 'admin',
     component: AdminComponent,
     canActivate: [AdminGuard],
-    children: [{ path: '', component: DashboardComponent }],
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'dashboard', component: DashboardComponent },
+      {
+        path: 'offers',
+        component: OffersComponent,
+      },
+      {
+        path: 'offers/:id',
+        component: OfferCrudComponent,
+      },
+      {
+        path: 'orders/:id',
+        component: OfferCrudComponent,
+      },
+    ],
   },
 ];
 
@@ -85,14 +107,22 @@ const ROUTES: Routes = [
     AuthOrderComponent,
     AdminComponent,
     DashboardComponent,
+    OffersComponent,
+    OfferCrudComponent,
+    OrdersComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(ROUTES),
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     MatSnackBarModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
