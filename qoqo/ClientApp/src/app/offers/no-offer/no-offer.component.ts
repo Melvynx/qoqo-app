bf2869544c6956a3c53f53893fd445536d21ec04
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-01-03 23:46:22 +0100

    feat(no-offer): add view for no offer

---
+++ qoqo/ClientApp/src/app/offers/no-offer/no-offer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-offer',
+  templateUrl: './no-offer.component.html',
+  styleUrls: ['./no-offer.component.css']
+})
+export class NoOfferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- qoqo/ClientApp/src/app/services/offer.service.ts
+++ qoqo/ClientApp/src/app/services/offer.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Offer } from 'src/types/offer';
 import { client } from '../../utils/client';
 import { ClickState } from '../../types/click';
+import {ActivatedRoute, Router, Routes} from '@angular/router';
 
 @Injectable({
   providedIn: 'platform',
@@ -9,21 +10,26 @@ import { ClickState } from '../../types/click';
 export class OfferService {
   offerEvent = new EventEmitter();
   offer?: Offer = undefined;
+  offerId?: string | null;
 
-  constructor() {
-    client<Offer>('offers/current')
-      .then((offer) => {
-        this.offer = offer;
-        client<ClickState>(`clicks/offers/${offer.id}`)
-          .then((result) => {
-            this.offerEvent.emit(result);
-          })
-          .catch((err) => {
-            console.error('err', err);
-          });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      this.offerId = paramMap.get('id') || 'current';
+
+      client<Offer>(`offers/${this.offerId}`)
+        .then((offer) => {
+          this.offer = offer;
+          client<ClickState>(`clicks/offers/${offer.id}`)
+            .then((result) => {
+              this.offerEvent.emit(result);
+            })
+            .catch((err) => {
+              console.error('err', err);
+            });
+        })
+        .catch((err) => {
+          router.navigateByUrl('/');
+        });
+    });
   }
 }
