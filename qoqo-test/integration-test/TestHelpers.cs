2568a740619a8a95e63665d953b83264426c599e
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-01-13 12:57:20 +0100

    add: test helpers

---
+++ qoqo-test/integration-test/TestHelpers.cs
@@ -0,0 +1,14 @@
+using System.Net.Http;
+using qoqo.Services;
+
+namespace qoqo_test.integration_test;
+
+public static class TestHelpers
+{
+    
+    public static T? GetBody<T>(HttpResponseMessage response)
+    {
+        var content = response.Content.ReadAsStringAsync().Result;
+        return JsonService.Deserialize<T>(content);
+    }
+}
